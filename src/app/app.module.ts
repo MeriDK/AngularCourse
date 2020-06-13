a0d154164f133ecd4c6b715ca7c5b2cd5f3bec36
MeriDK <ma.rizhko@gmail.com>
2020-06-13 16:23:01 +0300

    menu component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import 'hammerjs';
+import { MatLineModule } from '@angular/material/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatLineModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
