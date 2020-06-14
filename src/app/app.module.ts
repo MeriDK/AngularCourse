8769407f9dc79d929f0d8d3c6d3e9311732aff2b
MeriDK <ma.rizhko@gmail.com>
2020-06-14 11:44:52 +0300

    spa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -46,7 +47,8 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
